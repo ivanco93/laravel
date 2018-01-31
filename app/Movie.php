1b0778f20034ebec6c8999d77b8ac3421e2e1b2b
ivan.castiblanco <ivan.castiblanco@RM001883.gruporepremundo.loc>
2018-01-31 07:56:44 -0500

    Video 7 Curso Laravel 5

---
+++ app/Movie.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Cinema;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Movie extends Model
+{
+    //
+    protected $table = "movies";
+    //protected $primaryKey = "id";//Nombre de campo primary Key en Tabla
+    //public $timestamps = false;//Campos timestamps en tabla
+//    protected $fillable 
+}
